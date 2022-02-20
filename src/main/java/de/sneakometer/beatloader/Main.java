c4099bffa077486e52a9a57c12d8c8fc8baeb576
Sneakometer <sneakometer@gmail.com>
2022-02-20 15:37:53 +0100

    Add OpenFX

--- src/main/java/de/sneakometer/beatloader/Main.java
+++ src/main/java/de/sneakometer/beatloader/Main.java
@@ -1,9 +1,17 @@
 package de.sneakometer.beatloader;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world.");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
 	}
 
 }
